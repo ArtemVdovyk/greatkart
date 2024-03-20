619877328766d9a0552849c91e1dcdc9f020b5f8
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-03-20 13:07:59 +0200

    Added PayPal integration

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,29 @@
 import datetime
+import json
 from django.shortcuts import render, redirect
 
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 from carts.models import CartItem
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user,
+                              is_ordered=False,
+                              order_number=body["orderID"])
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=body["payment_method"],
+        amount_paid=order.order_total,
+        status=body["status"],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request, "orders/payments.html")
 
 
